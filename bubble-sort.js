167862546ba077143acd454d3bb11ca16eed92ef
levkowalski <levkowalski@gmail.com>
2017-12-31 01:31:24 +0100

    add bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,36 @@
+/**
+ * Explains the bubble sort concept
+ */
+function bubbleSortConcept(array) {
+  array = array.slice();
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+}
+
+/**
+ * Idiomatic bubble sort implementation
+ */
+function bubbleSort(array){
+  array = array.slice();
+  while (true) {
+    let swapped = false;
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return array;
+}
+
+const array = [4,3,2,1];
+
+console.log(bubbleSort(array));
